ab50f0544487b451a480c66747db2f93875091e6
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-10-18 22:19:35 +0200

    chore: create function deleteDuplicity

--- js/index.js
+++ js/index.js
@@ -2,10 +2,11 @@ let inputDom = document.getElementById("inputId");
 let btnShowDom = document.getElementById("btnShowId");
 let divDom = document.getElementById("divId");
 let btnSearchDom = document.getElementById("btnSearchId");
+let btnDuplicityDom = document.getElementById("btnDuplicityId");
 
 btnShowDom.addEventListener("click", getSentence);
 btnSearchDom.addEventListener("click", searchVocals);
-
+btnDuplicityDom.addEventListener("click", deleteDuplicity);
 
 function getSentence() {    
     phrase = inputDom.value
@@ -27,4 +28,23 @@ function searchVocals(){
     }
     divDom.innerHTML = "La frase contiene las siguientes vocales: " + JSON.stringify(newArrayPhrase)
     
+}
+
+function deleteDuplicity(){
+    phrase = inputDom.value
+    const vocals = ['a','e','i','o','u'];
+    let newArrayPhrase = [];
+    
+    for (let index = 0; index < phrase.length; index++) {        
+        if (vocals.includes(phrase[index]))
+        {   
+            if (!newArrayPhrase.includes(phrase[index]))
+                newArrayPhrase.push(phrase[index]);    
+            else{
+                console.log("ya la letra se encuentra")
+            }     
+        }
+    }
+    divDom.innerHTML = "La frase contiene las siguientes vocales: " + JSON.stringify(newArrayPhrase)
+
 }
